ce76c9312e335f1842f4da1335dcf21f9aa9e593
Zulal Toy <zulal.ty'@gmail.com>
2025-04-12 15:48:35 +0200

    entity modeling-some repos and service classes are added/implemented

---
+++ backend-cozy/src/main/java/com/cozycollections/backend_cozy/BackendCozyApplication.java
@@ -0,0 +1,13 @@
+package com.cozycollections.backend_cozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCozyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCozyApplication.class, args);
+	}
+
+}
